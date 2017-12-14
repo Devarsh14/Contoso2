1956901c7e801e013fc37116fa4141e5ad9f348a
Devarsh14 <Devarsh14@gmail.com>
2017-12-14 19:04:15 +1000

    Add project files.

---
+++ Contoso2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Contoso2.Startup))]
+namespace Contoso2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
