ce4592c222f6718c822b1a910b878ebefa78a96b
Devarsh14 <Devarsh14@gmail.com>
2017-12-15 17:48:30 +1000

    Course controller added and Detaisl page succesfully created.

---
+++ Contoso2/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using Contoso2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contoso2.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ApplicationDbContext db= new ApplicationDbContext();
+        // GET: Course
+        public ActionResult Index()
+        {
+            var courselist = db.Courses.ToList();
+
+
+            return View(courselist);
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            var CourseDetails = db.Courses.SingleOrDefault(c=>c.CourseID==id);
+            return View(CourseDetails);
+        }
+    }
+}
